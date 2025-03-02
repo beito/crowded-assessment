7189c15818b108240920d5f2f29bc2a3d8936f8c
Julio <beito_xp@me.com>
2025-03-01 19:02:29 -0600

    Adding Database Configuration

---
+++ src/database/database.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest_installments',
+      autoLoadModels: true,
+      synchronize: true
+    }),
+  ],
+  exports: [SequelizeModule],
+})
+export class DatabaseModule {}
